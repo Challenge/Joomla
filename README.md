de058607ad023d0431c8957ec0af5609e2383af8
Ektorus <ulriksaa@gmail.com>
2014-03-12 23:05:33 +0100

    Updated the README to state that the script requires zip to be installed. Also updated the script to check that zip is actually installed.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Joomla
 ======
 This repository contains the files need for joomla (like components) which are not directly located on the homepage.
+
+To package all the Joomla components into zip files (so they can be installed in Joomla) run ./build_joomla-component.sh from the root directory.
+The script requires the normal linux base system and also relies on the program 'zip' to package the files into zip-format.
+To install this program refer to the installation guide for your linux distribution.
--- build_joomla-component.sh
+++ build_joomla-component.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
+### Check for dependencies
+# This script relies on the normal linux base system and the following programs:
+DEPENDENCIES="zip"
+
+for dep in $DEPENDENCIES
+do
+    if ! which $dep > /dev/null 2>&1
+    then
+        echo "This script requires '$dep' to run but it is not installed."
+        echo "Please install the required program and re-run the script."
+        exit 1
+    fi
+done
+
 ### Global variables
 BUILD_FOLDER="builds"
 COMPONENT_START_NAME="com_"
 
+# Get the absolute path to the folder where the script resides
+SCRIPT_PATH=$(dirname $(readlink -f $0))
+
 
 ### Initialize
 # Create build folder
-mkdir -p ${BUILD_FOLDER}
+mkdir -p ${SCRIPT_PATH}/${BUILD_FOLDER}
 
 
 ### Main
 # Loop through all files in the directory
-for f in *; do
+for f in ${SCRIPT_PATH}; do
 
     # File is a directory and starts with $COMPONENT_START_NAME
     if [[ -d ${f} ]] && [[ ${f} == "$COMPONENT_START_NAME"* ]]; then
@@ -22,7 +39,7 @@ for f in *; do
 
         # Zip Joomla components
         echo -n "Zipping ${f}..."
-        zip -r "${BUILD_FOLDER}/${f}-${version}.zip" "${f}" > /dev/null 2>&1
+        zip -r "${SCRIPT_PATH}/${BUILD_FOLDER}/${f}-${version}.zip" "${SCRIPT_PATH}/${f}" > /dev/null 2>&1
         echo "Done"
 
     fi
